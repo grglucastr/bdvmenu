42f8f41651ada269d79b5e23a4f4ef294d124955
George Lucas T. Bentes <george.bentes@gmail.com>
2017-09-09 11:33:07 -0400

    Categorias start page

---
+++ categorias.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+<h5>Categorias</h5>
+
+<p>
+    <a href="index.php" title="Voltar para o Menu Principal">Menu Principal</a>
+</p>
+
+<ul>
+    <li><a href="categorias_nova.php">Inserir Nova</a></li>
+</ul>
+
+
+<?php include 'footer.php'; ?>
