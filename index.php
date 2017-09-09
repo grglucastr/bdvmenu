bd5a4b14e203e61f8bb7a81c2bd2990009eea5a9
George Lucas T. Bentes <george.bentes@gmail.com>
2017-09-09 11:34:05 -0400

    Index menu

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+
+<h3>Painel administrador</h3>
+<h5>Início</h5>
+<ul>
+    <li> <a href="#">Início</a> </li>
+    <li> <a href="programacao.php">Programação Cardápio</a></li>
+    <li> <a href="itens.php">Itens</a></li>
+    <li> <a href="categorias.php">Categorias</a></li>
+    <li> <a href="unidades.php">Unidades</a></li>
+</ul>
+
+<?php include 'footer.php'; ?>
