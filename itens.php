dc6a5ca942acde4dcf0ccddbc113e44f57988da0
George Lucas T. Bentes <george.bentes@gmail.com>
2017-09-17 13:39:27 -0400

    Pagina principal de itens

--- itens.php
+++ itens.php
@@ -1,2 +1,10 @@
 <?php include 'header.php'; ?>
+<h5>Itens do Cardápio</h5>
+
+<p><a href="index.php" title="Voltar para o Menu Principal">Menu Principal</a></p>
+<ul><li><a href="itens_form.php">Inserir Nova</a></li></ul>
+
+<h6>Lista de Itens</h6>
+
+
 <?php include 'footer.php'; ?>
